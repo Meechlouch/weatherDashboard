bbb18069b3e47d4c5765dbbc7861c204cafb3604
Dj Meechlouch <Meechlouch@gmail.com>
2020-11-06 22:15:46 -0600

    can now get response from api for 5 day forecast

--- weatherDashboard.js
+++ weatherDashboard.js
@@ -1,47 +1,74 @@
 $(document).ready(function() {
-
-let city = $("#city").val();
-let apiKey = "9ee1e37fcd420a11ca0592d62f5fa2ce";
-
-$("#searchCities").on("click", function() {
     
-    city = $("#city").val();
-    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + apiKey;
+    //Declare Global variables
+    let city = $("#city").val();
+    let apiKey = "9ee1e37fcd420a11ca0592d62f5fa2ce";
+    //let currentTemp = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+    
+    $("#searchCities").on("click", function() {
+    
+        city = $("#city").val();
+        let currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&appid=" + apiKey;
 
-    if(city != "") {
+        if(city != "") {
         
-        $.ajax({
-            url: queryUrl,
-            type: "GET",
-        }).then(function(response) {
-            console.log(response);
-            let displayContent = show(response);
-            $("#currentForecast").html(displayContent);
-        });
-    } else {
-        console.log("error");
+            $.ajax({
+                url: currentWeather,
+                type: "GET",
+            }).then(function(response) {
+                console.log(response);
+                let displayContent = show(response);
+                $("#currentForecast").html(displayContent);
+                //fiveDayForecast();
+                displayHistory();
+            });
+        } else {
+            console.log("error");
+        }
+    });
+
+    function show(response) {
+        let momentJS = moment().subtract(10, 'days').calendar();
+        let UVindex = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=lat&lon=lon";
+
+        return  `<h4><strong>${response.name}</strong></h4>${momentJS}<img src='http://openweathermap.org/img/wn/${response.weather[0].icon}.png'><br>Temperature: ${response.main.temp.toFixed(1)} &deg;F</br>Humidity: ${response.main.humidity}%<br>Wind Speed: ${response.wind.speed} MPH<br>UV Index: ${response.coord.lon}`
     }
-});
 
-});
+    function fiveDayForecast() {
+        city = $("#city").val();
+        let fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&exclude=daily" + "&appid=" + apiKey;
 
-function show(response) {
+        $.ajax({
+            url: fiveDay,
+            type: "GET",
+        }).then(function(data) {
+            console.log(data.list.dt);
+            $("#fiveDayForecast").html(data);
+        });
+    }
 
-    return  "<h4><strong>" + response.name + "</strong></h4>" + "<img src='http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png'>" + "<br>" +
-            "Temperature: " + response.main.temp.toFixed(1) + " &deg;F"  + "</br>" +
-            "Humidity: " + response.main.humidity + "%" + "<br>" +
-            "Wind Speed: " + response.wind.speed + " MPH" + "<br>" +
-            "UV Index: "
-}
-
-function  displayHistory() {
-    let liEl = $("<li>");
-    let inputEl = $("<input>");
-    city = $("#city").val();
+    function  displayHistory() {
+        
+        let liEl = $("<li>");
+        city = $("#city").val();
         $("#searchHistory").append(liEl);
         liEl.append(inputEl);
+        //inputEl.html(city);
         inputEl.html(city);
         console.log(city);
         //$("#searchHistory").prepend(liEl);
-    
-};
+    }
+
+    /*
+
+    let fiveDayApiCall = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+
+    $.ajax({
+        url: fiveDayApiCall,
+        type: "GET",
+    }).then(function(response) {
+        console.log(response);
+
+    });
+    */
+});
