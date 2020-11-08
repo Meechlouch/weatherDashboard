30732ef68672110b59d7e410aadacd02d70dad21
Dj Meechlouch <Meechlouch@gmail.com>
2020-11-07 20:12:57 -0600

    successfully displayed 5 day temp and humidity

--- weatherDashboard.js
+++ weatherDashboard.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
     let history = JSON.parse(window.localStorage.getItem("history")) || [];
     for (let i = 0; i < history.length; i++) {
         displayHistory(history[i]);
+        if (history.length > 9) {
+            //console.log(history.shift());
+            history.shift();
+        }
       }
-
-    if (history.length > 8) {
-        console.log(history.pop());
-    }
 
       $("#searchHistory").on("click", "li", function() {
           city = $(this).text();
@@ -45,9 +45,10 @@ $(document).ready(function() {
             url: currentWeather,
             type: "GET",
         }).then(function(response) {
-            console.log(response);
+            //console.log(response);
+           // console.log("This apiCall gives current weather for city input value");
             getUV(response.coord.lat, response.coord.lon, response)
-  
+            
         });
     }
 
@@ -57,20 +58,16 @@ $(document).ready(function() {
             url: uvIndexUrl,
             type: "GET",
         }).then(function(uvResp) {
-            console.log(uvResp);
+            //console.log(uvResp);
+            //console.log("this apiCall gives lat and lon");
             let displayContent = show(weatherData,uvResp.value);
             $("#currentForecast").html(displayContent);
-            
-
-            fiveDayForecast();
-            
+            fiveDayForecast(uvResp.lat, uvResp.lon); 
         });
     }
 
-
-
     function show(response, uvValue) {
-        let momentJS = moment().subtract(10, 'days').calendar();
+        let momentJS = moment().format("MMM Do YY");
         let color = null;
         if(uvValue < 3){
             color = 'btn btn-success';
@@ -83,18 +80,39 @@ $(document).ready(function() {
         return  `<h4><strong>${response.name}</strong></h4>${momentJS}<img src='http://openweathermap.org/img/wn/${response.weather[0].icon}.png'><br>Temperature: ${response.main.temp.toFixed(1)} &deg;F</br>Humidity: ${response.main.humidity}%<br>Wind Speed: ${response.wind.speed} MPH<br>UV Index: <span class='${color}'>${uvValue}</span> `
     }
 
-    function fiveDayForecast() {
+    function fiveDayForecast(lat, lon) {
         
-        let fiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&exclude=daily" + "&appid=" + apiKey;
-
+        let fiveDay = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&appid=" + apiKey;
+        //console.log(fiveDay);
         $.ajax({
             url: fiveDay,
             type: "GET",
-        }).then(function(data) {
-            console.log(data);
-            for(i = 0; i < data.list.length; i++) {
-            $("#fiveDayForecast").html(data.list[0].main.temp[i]);
-            }
+        }).then(function(response) {
+           console.log(response);
+           // console.log("apiCall five Day");
+            //let dt = data.list[0].dt;
+            //(moment.unix(dt));
+            
+                //let momentJS = moment().format("MMM Do YY");
+                
+                //divEl.setAttribute("style", "width: 10px;");
+                for (let i = 0; i < 5; i++) {
+                    let divEl = $("<div>");
+                    let fiveTemp = response.daily[i].temp.day;
+                    let fiveHumid = response.daily[i].humidity;
+
+                    let fiveTempPara = $("<p>").text("Temp: " + fiveTemp.toFixed(1));
+                    let fiveHumidPara = $("<p>").text("Humidity: " + fiveHumid);
+
+
+                    divEl.addClass("card");
+                    divEl.append(fiveTempPara);
+                    divEl.append(fiveHumidPara);
+                    console.log(response.daily[i].temp.day);
+                    console.log("full object");
+                    $("#fiveDayForecast").append(divEl);
+                }
+           
         });
     }
 
